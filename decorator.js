8ab0053ab77f1d53cdb800d4447eeb3c120e5852
rizqy.imceria <rizqy@imceria.com>
2023-02-07 11:41:36 +0700

    decorator with call

--- decorator.js
+++ decorator.js
@@ -1,38 +1,40 @@
 let worker = {
-  someMethod() {
-    return 1
-  },
-  slow(x) {
+  slow(max, min) {
     return new Promise(resolve => {
       setTimeout(() => {
-        resolve(x * this.someMethod())
+        resolve(min + max)
       }, 3000)
     })   
   }
 }
 
-function cachingDecorator(func) {
+function cachingDecorator(func, hash) {
   const cache = new Map()
-  return function(x) {
-    if (cache.get(x)) {
-      return cache.get(x)
+  return function() {
+    const key = hash(arguments)
+    if (cache.get(key)) {
+      return cache.get(key)
     }
 
-    const result = func.call(this, x)
-    cache.set(x, result)
+    const result = func.call(this, ...arguments)
+    cache.set(key, result)
 
     return result
   }
 }
 
-worker.slow = cachingDecorator(worker.slow)
+function hash(args) {
+  return args[0] + ' ' + args[1]
+}
+
+worker.slow = cachingDecorator(worker.slow, hash)
 
 async function callWorker() {
-  const result1 = await worker.slow(1)
+  const result1 = await worker.slow(1, 10)
 
   console.log(result1)
 
-  const result2 = await worker.slow(1)
+  const result2 = await worker.slow(1, 10)
 
   console.log(result2)
 }
